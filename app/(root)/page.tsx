9ce5fb0162f393e39b2cc75ae302a5e66b4dcbcb
Parth Sharma <parthsharma@csu.fullerton.edu>
2025-02-24 09:27:16 -0800

    phew the login/signup are working now

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,30 @@
-import Image from "next/image";
-import { Button } from "@/components/ui/button"
+import BookList from "@/components/BookList";
+import BookOverview from "@/components/BookOverview";
+import { db } from "@/database/drizzle";
+import { books, users } from "@/database/schema";
+import { auth } from "@/auth";
+import { desc } from "drizzle-orm";
 
-const Home = () => {
-  
-    <div>
-      <Button>Click me</Button>
-    </div>
+const Home = async () => {
+  const session = await auth();
 
-}
+  const latestBooks = (await db
+    .select()
+    .from(books)
+    .limit(10)
+    .orderBy(desc(books.createdAt))) as Book[];
 
-export default Home
+  return (
+    <>
+      <BookOverview {...latestBooks[0]} userId={session?.user?.id as string} />
+
+      <BookList
+        title="Latest Books"
+        books={latestBooks.slice(1)}
+        containerClassName="mt-28"
+      />
+    </>
+  );
+};
+
+export default Home;
